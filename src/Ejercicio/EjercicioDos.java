5404f105798b6bc1ebc0c89ab828e71236e0ef0e
Ricardo <ricardoromero4679@gmail.com>
2024-07-15 21:45:05 -0500

    Ejercicio en clases

--- src/Ejercicio/EjercicioDos.java
+++ src/Ejercicio/EjercicioDos.java
@@ -1,5 +1,9 @@
 package Ejercicio;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 /*
  * Dados n pares de paréntesis, implementa un algoritmo para generar todas las combinaciones
  * válidas de paréntesis.
@@ -21,7 +25,31 @@ package Ejercicio;
  */
 public class EjercicioDos {
 
-    // List<List<Integer>> subsets(List<Integer> set) {
+    public List<String> resultadoEjercicio(int n) {
+        List<String> resultado = new ArrayList<>();
+        resultadoEjercicio(new StringBuilder(), n, n, resultado);
+        return resultado;
+    }
 
-    // }
+    private void resultadoEjercicio(StringBuilder current, int open, int close, List<String> resultado) {
+        if (open == 0 && close == 0) {
+            resultado.add(current.toString());
+            return;
+        }
+
+        if (open > 0) {
+            current.append('(');
+            resultadoEjercicio(current, open - 1, close, resultado);
+            current.deleteCharAt(current.length() - 1);
+        }
+
+        if (close > open) {
+            current.append(')');
+            resultadoEjercicio(current, open, close - 1, resultado);
+            current.deleteCharAt(current.length() - 1);
+      
+    
+        }  
+    }
+
 }
--- src/Ejercicio/EjercicioUno.java
+++ src/Ejercicio/EjercicioUno.java
@@ -1,6 +1,7 @@
 package Ejercicio;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /*
@@ -28,8 +29,30 @@ import java.util.List;
  * Ecoja la mejor estructura de datos para que sea mas eficiente
  */
 public class EjercicioUno {
+    public List<List<Integer>> subsets(List<Integer> set) {
+        HashSet<List<Integer>> resultado = new HashSet<>();
+        int num = set.size();
+        int numeroSubsets = calcularNumeroSubsets(num);
 
-    // List<List<Integer>> subsets(List<Integer> set) {
+        for (int i = 0; i < numeroSubsets; i++) {
+            List<Integer> subset = new ArrayList<>();
+            int act = i;
+            for (int j = 0; j < num; j++) {
+                if (act % 2 == 1) {
+                    subset.add(set.get(j));
+                }
+                act /= 2;
+            }
+            resultado.add(subset);
+        }
+        return new ArrayList<>(resultado);
+    }
 
-    // }
+    public int calcularNumeroSubsets(int num) {
+        int result = 1;
+        for (int i = 0; i < num; i++) {
+            result *= 2;
+        }
+        return result;
+    }
 }
